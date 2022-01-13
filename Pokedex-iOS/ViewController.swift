26217d5ec5f1e2023b0a34ab8acf0b2bf410face
EstrHuP <esthrhup@gmail.com>
2022-01-13 18:32:53 +0100

    first commit

---
+++ Pokedex-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex-iOS
+//
+//  Created by Esther Huecas on 13/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
